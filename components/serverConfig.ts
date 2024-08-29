5c1119ab5c02205a0fedd5a8c0072b3bbd5d7b18
Ahmad Alhomsi <tarikuka.aa@gmail.com>
2024-08-29 22:34:09 +0300

    getServerBaseUrl: adding the development address

--- components/serverConfig.ts
+++ components/serverConfig.ts
@@ -1,3 +1,9 @@
-const serverBaseUrl = 'http://162.19.152.156:3000';
-
-export { serverBaseUrl };
+function getServerBaseUrl() {
+    if (process.env.NODE_ENV === 'development') {
+      return 'http://localhost:3000';
+    } else {
+      return 'http://162.19.152.156:3000';
+    }
+  }
+  
+  export const serverBaseUrl = getServerBaseUrl();
