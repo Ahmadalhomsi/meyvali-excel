3c0d75abed55d97e2789201fa8e1f104afc234f2
Ahmad Alhomsi <tarikuka.aa@gmail.com>
2024-09-06 19:41:00 +0300

    Server Config domain fix

--- components/serverConfig.ts
+++ components/serverConfig.ts
@@ -2,7 +2,7 @@ function getServerBaseUrl() {
     if (process.env.NODE_ENV === 'development') {
       return 'http://localhost:3000';
     } else {
-      return 'https://meyvalilokantasi.com/'; // http://37.148.211.114:3000
+      return 'https://meyvalilokantasi.com'; // http://37.148.211.114:3000
     }
   }
   
