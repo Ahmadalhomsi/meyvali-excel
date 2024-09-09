3257dbd7168c222482e69862845683bf79f8553c
Ahmad Alhomsi <tarikuka.aa@gmail.com>
2024-09-09 15:03:20 +0300

    Undoing Server Base URL

--- components/serverConfig.ts
+++ components/serverConfig.ts
@@ -1,5 +1,9 @@
 function getServerBaseUrl() {
+    if (process.env.NODE_ENV === 'development') {
       return 'http://localhost:3000';
+    } else {
+      return 'https://meyvalilokantasi.com'; // http://37.148.211.114:3000
+    }
   }
   
   export const serverBaseUrl = getServerBaseUrl();
